bf0c6afc2ca4321bb4d558be382f4d62b59ad8e4
maximusdionyssopoulos <maximusdionyssopoulos@gmail.com>
2025-02-13 16:34:32 +1100

    Initial Commit

---
+++ PlugKit/PlugKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlugKitApp.swift
+//  PlugKit
+//
+//  Created by Maximus Dionyssopoulos on 13/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlugKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
